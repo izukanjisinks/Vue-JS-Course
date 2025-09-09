39869f23f71fc9ab4202e848875abb6cfa0fc373
izukanji <izukanjisinkolongo@gmail.com>
2025-09-09 09:26:41 +0200

    adding and integrating the router package

--- section-13-routing/src/components/nav/TheNavigation.vue
+++ section-13-routing/src/components/nav/TheNavigation.vue
@@ -3,26 +3,16 @@
     <nav>
       <ul>
         <li>
-          <button @click="setActivePage('teams-list')">Teams</button>
+          <router-link to="/teams">Teams</router-link>
         </li>
         <li>
-          <button @click="setActivePage('users-list')">Users</button>
+          <router-link to="/users">Users</router-link>
         </li>
       </ul>
     </nav>
   </header>
 </template>
 
-<script>
-export default {
-  emits: ['set-page'],
-  methods: {
-    setActivePage(page) {
-      this.$emit('set-page', page);
-    },
-  },
-};
-</script>
 
 <style scoped>
 header {
@@ -48,9 +38,11 @@ ul {
 li {
   margin: 0 2rem;
 }
-
-button {
-  font: inherit;
+/* the anchor tag this styling points to is actually
+   router-link because under the hood router-link uses 
+   an anchor tag */
+a {
+  text-decoration: none;
   background: transparent;
   border: 1px solid transparent;
   cursor: pointer;
